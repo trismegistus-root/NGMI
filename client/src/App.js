0332d86df852444edd893b4a8ebe0892860ee725
unknown <edward.baldwin@ucdenver.edu>
2021-01-27 02:58:51 -0700

    The rest of the files..in progress

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+//props: drizzle = {drizzle} from drizzle/state
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
