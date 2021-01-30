51b90cab370bdbebaf7dab93da209f4a12a6b3b8
unknown <edward.baldwin@ucdenver.edu>
2021-01-30 01:03:37 -0700

    ...

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ngmi from './contracts/ngmi.json';
 import "./App.css";
 import WhitePaper from "./components/WhitePaper/WhitePaper.js";
+import Web3 from 'web3';
+
+let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
 
 
 class App extends React.Component{
--- contracts/ngmi.sol
+++ contracts/ngmi.sol
@@ -1,5 +1,5 @@
-
-	pragma solidity >=0.4.0 < 0.8.0;  
+//SPDX-License-Identifier: <SPDX-License>
+	pragma solidity >=0.4.0 < 0.8.1;  
 	
 	import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 	import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
