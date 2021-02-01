75ad5be5cb677dbdc4210382a08894ac34759780
trismegistus-root <edward.baldwin@ucdenver.edu>
2021-02-01 13:46:17 -0700

    ...

--- truffle-config.js
+++ truffle-config.js
@@ -17,14 +17,15 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
+   require('dotenv').config();
+   const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-  //const HDWalletProvider = require('@truffle/hdwallet-provider');
-  //const infuraKey = "d26013c8263c4aa59983193291770d3c";
+
+   // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
- //const mnemonic = "mouse amused jacket reopen keen where party bring photo mask mimic degree oval name lounge rare quality behind bulk airport diesel burden please spot";
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
 const path = require("path")
-
 
 module.exports = {
   /**
@@ -36,7 +37,8 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-   networks: {
+
+  networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
@@ -44,10 +46,10 @@ module.exports = {
     // options below to some value.
     //
      development: {
-      	host: "127.0.0.1",     // Localhost (default: none)
-     	port: 7545,            // Standard Ethereum port (default: none)
-    	network_id: "5777",       // Any network (default: none)
-    },
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "5777",       // Any network (default: none)
+     },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -59,20 +61,22 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-      /* ropsten: {
-       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/d26013c8263c4aa59983193291770d3c`),
-       network_id: 3,       // Ropsten's id
-       gas: 5500000,        // Ropsten has a lower block limit than mainnet
-       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-       },*/
+     ropsten: {
+     provider: function(){
+         return new HDWalletProvider(process.env.MNEMONIC,
+      'https://ropsten.infura.io/${process.env.INFURA_API_KEY}')},
+     network_id: 3,       // Ropsten's id
+     gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+     },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
-    // },
+    // }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -83,16 +87,16 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-         version: "0.5.0",    // Fetch exact version from solc-bin (default: truffle's version)
+       version: "0.5.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
       //  evmVersion: "byzantium"
       // }
     }
   },
-   contracts_build_directory: path.join(__dirname, "app/src/contracts")
+  contracts_build_directory: path.join(__dirname, "app/src/contracts")
 };
